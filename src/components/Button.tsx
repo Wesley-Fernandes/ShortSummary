58fb2c8da8f7e2c0039bb427dedf45b6c2d3d0a1
Wesley Fernandes <wesleyintelligence@protonmail.com>
2023-09-19 06:26:10 -0300

    feature: button animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button() {
     <button
       type="submit"
       disabled={status != "pausado"}
-      className="w-[48px] h-[48px] transition-colors delay-150 ease-in-out rounded flex items-center justify-center text-white bg-purple-1 hover:bg-purple-1/40 disabled:bg-purple-1/40"
+      className="animate__animated animate__fadeInRight w-[48px] h-[48px] transition-colors delay-150 ease-in-out rounded flex items-center justify-center text-white bg-purple-1 hover:bg-purple-1/40 disabled:bg-purple-1/40"
     >
       <Play />
     </button>
